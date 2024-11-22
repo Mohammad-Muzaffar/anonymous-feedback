01e6901d1606d646f861879f5b5bce4301d7411a
muzaffar-angularminds <muzaffar@angularminds.in>
2024-11-22 18:13:29 +0530

    feat: added dynamic api route for getting post by id

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -3,29 +3,57 @@ import { authOptions } from "../../auth/[...nextauth]/options";
 import { NextResponse } from "next/server";
 import dbConnect from "@/models/db.connect";
 import PostModel from "@/models/post.model";
-import { postSchema } from "@/schemas/postSchema";
-import mongoose from "mongoose";
+import FeedbackModel from "@/models/feedback.model";
 
 export async function GET(request: Request) {
-  await dbConnect();
-
-  // Get the user session
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!session || !user) {
-    return NextResponse.json(
-      { success: false, message: "Not authenticated." },
-      { status: 401 }
-    );
-  }
-
-  const userId = new mongoose.Types.ObjectId(user._id);
-
   try {
-    // TODO.
+    await dbConnect();
+
+    // Get the user session
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "Not authenticated." },
+        { status: 401 }
+      );
+    }
+
+    const { pathname } = new URL(request.url);
+    const id = pathname.split("/").pop(); // Extract dynamic ID
+
+    if (!id) {
+      return NextResponse.json(
+        { success: false, message: "Post ID not provided." },
+        { status: 400 }
+      );
+    }
+
+    const userId = user._id;
+    const postPromise = PostModel.findOne({ userId, _id: id }).select(
+      "title description likes dislikes isAcceptingFeedback createdAt"
+    );
+    const feedbackPromise = FeedbackModel.find({ postId: id }).select(
+      "content createdAt votes"
+    );
+
+    // Resolve both queries concurrently
+    const [post, feedback] = await Promise.all([postPromise, feedbackPromise]);
+
+    if (!post) {
+      return NextResponse.json(
+        { success: false, message: "Post not found." },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, post, feedback },
+      { status: 200 }
+    );
   } catch (error) {
-    console.error("Error while retrieving posts:", error);
+    console.error("Error while retrieving post:", error);
 
     return NextResponse.json(
       {
