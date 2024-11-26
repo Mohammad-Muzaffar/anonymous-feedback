dd59a909f0d7262dd05dcece4b93a2ffcd8a0857
muzaffar-angularminds <muzaffar@angularminds.in>
2024-11-26 13:02:56 +0530

    feat: started frontend completed signup page:

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,13 +5,19 @@ import zod from "zod";
 import Link from "next/link";
 import { useState } from "react";
 import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 const Page: React.FC = () => {
   const [username, setUsername] = useState("");
   const [usernameMessage, setUsernameMessage] = useState("");
+  
   const [isCheckingUsername, setIsCheckingUsername] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
+
   const debouncedUsername = useDebounceValue(username, 350);
+  const { toast } = useToast();
+  const router = useRouter();
 
   return <div>hi</div>;
 };
